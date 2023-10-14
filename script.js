ea72c83214653b3bd745f1c1643c8eb876af1881
A R <12204041+iravella@users.noreply.github.com>
2023-10-14 20:25:16 +0200

    add search

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 // Constants
-const SUNNY_MESSAGE =
+/* const SUNNY_MESSAGE =
   "Get your sunnies on. Stockholm is looking rather great today.";
 const RAINY_MESSAGE =
-  "Don’t forget your umbrella. It’s wet in Stockholm today.";
+  "Don’t forget your umbrella. It’s wet in ${city} today.";
 const CLOUDY_MESSAGE =
-  "Light a fire and get cosy. Stockholm is looking grey today.";
+  "Light a fire and get cosy. Stockholm is looking grey today."; */
+
+let city = "Stockholm";
+//const API_KEY = "736699be9817dd8fcbc05fed82aa17ed";
+
+const API_KEY = "7309e4a5829fafe809df835ad95f18ea";
 
 const container = document.getElementById("container");
 const currentWeather = document.getElementById("currentWeather");
 const weatherImg = document.getElementById("weatherImg");
 const weatherText = document.getElementById("weatherText");
+const searchButton = document.getElementById("searchButton");
+const searchInput = document.getElementById("searchInput");
+
+function setCity(cityName) {
+	city = cityName;
+	searchInput.value = cityName;
+  }
+  
 
 function setImgSrc(src) {
   console.log("hej hej");
@@ -26,79 +39,71 @@ function setSunTimeParagraph(sunTime, milliSeconds) {
 }
 
 function setWeatherClass(weatherClass) {
-	switch (weatherClass) {
-		case "clear":
-			container.classList.toggle("clear");
-			setImgSrc("sunglasses");
-			weatherText.innerText = `${SUNNY_MESSAGE}`;
-			break;
-		case "rain":
-			container.classList.toggle("rain");
-			setImgSrc("umbrella");
-			weatherText.innerText = `${RAINY_MESSAGE}`;
-			break;
-		case "cloudy":
-		case "clouds":
-			container.classList.toggle("cloudy");
-			setImgSrc("cloud");
-			weatherText.innerText = `${CLOUDY_MESSAGE}`;
-			break;
-
-		default:
-			container.classList.toggle("default");
-	}
+  switch (weatherClass) {
+    case "clear":
+      container.classList.toggle("clear");
+      setImgSrc("sunglasses");
+      weatherText.innerText = `Get your sunnies on. ${city} is looking rather great today.`;
+      break;
+    case "rain":
+    case "drizzle":
+      container.classList.toggle("rain");
+      setImgSrc("umbrella");
+      weatherText.innerText = `Don't forget your umbrella. It's wet in ${city} today.`;
+      break;
+    case "cloudy":
+    case "clouds":
+      container.classList.toggle("cloudy");
+      setImgSrc("cloud");
+      weatherText.innerText = `Light a fire and get cosy. ${city} is looking grey today.`;
+      break;
+    default:
+      container.classList.toggle("default");
+  }
 }
 
-async function getWeather() {
-  //   try {
-  //     const response = await fetch(
-  //       "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=162e87975bc70dc88548f3920f1c4fdf"
-  //     );
-  //     const weather = await response.json();
-
-  //     const li = document.createElement("li");
-  //     li.innerHTML += `
-  //   			<div>
-  //   				<h2>
-  //   					${weather.name}
-  //   				</h2>
-  //   				<p>Temperature ${weather.main.temp.toFixed(1)}</p>
-  //   				<p>Description: ${weather.weather[0].description}</p>
-  //   			</div>`;
-
-  //     weatherContainer.appendChild(li);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
+async function getWeather(city) {
   try {
     const response = await fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=162e87975bc70dc88548f3920f1c4fdf"
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_KEY}`
     );
     const weatherResponse = await response.json();
-    const weatherClass = weatherResponse.weather[0].main;
-    const currentTemperature = Math.round(weatherResponse.main.temp);
+    //if (weatherResponse.weather && weatherResponse.weather.length > 0) {
+      const weatherClass = weatherResponse.weather[0].main;
+      const currentTemperature = Math.round(weatherResponse.main.temp);
 
-    const weatherParagraph = document.createElement("p");
-    weatherParagraph.innerHTML += `${weatherClass} | ${currentTemperature}&#176;`;
-    currentWeather.appendChild(weatherParagraph);
+	  currentWeather.innerHTML = ""; // Clear existing content
+      const weatherParagraph = document.createElement("p");
+      weatherParagraph.innerHTML += `${weatherClass} | ${currentTemperature}&#176;`;
+      currentWeather.appendChild(weatherParagraph);
 
-    setSunTimeParagraph("sunrise", weatherResponse.sys.sunrise);
-    setSunTimeParagraph("sunset", weatherResponse.sys.sunset);
-    setWeatherClass(weatherClass.toLowerCase());
+      setSunTimeParagraph("sunrise", weatherResponse.sys.sunrise);
+      setSunTimeParagraph("sunset", weatherResponse.sys.sunset);
+      setWeatherClass(weatherClass.toLowerCase());
+	  //city = weatherResponse.name;
+
+	  setCity(weatherResponse.name); // Update the city variable with the actual city name
+
+    //} else {
+    //   currentWeather.innerHTML = "No weather data available.";
+    // }
   } catch (err) {
     console.error(err);
-    currentWeather.innerHTML += `Could not get today's weather`;
+    currentWeather.innerHTML += `Could not get today's weather for ${city}`;
   }
 }
 
 const forecastContainer = document.getElementById("forecast-container");
-const forecastURL =
-  "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=693dd010904bcc0402e15bf40afeee28";
+//const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=${API_KEY}`;
 
-async function getForecast() {
+async function getForecast(city) {
+	const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=${API_KEY}`;
+
   try {
     const response = await fetch(forecastURL);
     const forecast = await response.json();
+
+	forecastContainer.innerHTML = ""; // Clear existing content
 
     const noonWeather = forecast.list.filter((obj) =>
       obj.dt_txt.includes("12:00:00")
@@ -119,8 +124,19 @@ async function getForecast() {
   }
 }
 
-// search funtion here
+searchButton.addEventListener("click", () => {
+  const newCity = searchInput.value.trim();
+  if (newCity) {
+	//setCity(newCity);
+	//city = newCity; 
+	//searchInput.value = city;
+	getWeather(newcity);
+	getForecast(newcity);
+  } else {
+	console.log("Please enter a city name.");
+  }
+});
 
 // Function calls here
-getForecast();
-getWeather();
+getWeather(city);
+getForecast(city);
