79a7602bda320d881ef841fac4825e3c3f97f05b
Disabledbarrel <elin.elisabeth.larsson@gmail.com>
2023-10-15 12:49:59 +0200

    Search and display city

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-// Constants
-//  const SUNNY_MESSAGE =
-// 	"Get your sunnies on. Stockholm is looking rather great today.";
-// const RAINY_MESSAGE =
-// 	"Don’t forget your umbrella. It’s wet in Stockholm today.";
-// const CLOUDY_MESSAGE =
-// 	"Light a fire and get cosy. Stockholm is looking grey today.";
-
-//const forecastURL =
-//	"https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=693dd010904bcc0402e15bf40afeee28";
-
-let city = "Stockholm";
+let city = "Stockholm"; // default city to start with
 const API_KEY = "7309e4a5829fafe809df835ad95f18ea";
-
-const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=${API_KEY}`;
 
 // DOM elements
 const container = document.getElementById("container");
@@ -21,126 +8,121 @@ const weatherImg = document.getElementById("weatherImg");
 const weatherText = document.getElementById("weatherText");
 const forecastContainer = document.getElementById("forecast-container");
 
-const searchButton = document.getElementById("searchButton");
-const searchInput = document.getElementById("searchInput");
-
 function setImgSrc(src) {
-  weatherImg.src = `img/${src}.svg`;
-  weatherImg.alt = src;
-}
-
-function setCity(cityName) {
-  city = cityName;
-  searchInput.value = cityName; // Update the input field
+	weatherImg.src = `img/${src}.svg`;
+	weatherImg.alt = src;
 }
 
 // Feature: Sunrise and sunset
 function setSunTimeParagraph(sunTime, milliSeconds) {
-  const timeInHours = new Date(milliSeconds * 1000)
-    .toLocaleTimeString("sv-SE", { hour: "2-digit", minute: "2-digit" })
-    .replace(":", ".");
-  const sunTimeParagraph = document.createElement("p");
-  sunTimeParagraph.innerHTML += `${sunTime} ${timeInHours}`;
-  currentWeather.appendChild(sunTimeParagraph);
+	const timeInHours = new Date(milliSeconds * 1000)
+		.toLocaleTimeString("sv-SE", { hour: "2-digit", minute: "2-digit" })
+		.replace(":", ".");
+	const sunTimeParagraph = document.createElement("p");
+	sunTimeParagraph.innerHTML += `${sunTime} ${timeInHours}`;
+	currentWeather.appendChild(sunTimeParagraph);
 }
 
-function setWeatherClass(weatherClass) {
-  const weather = weatherClass.split(" ");
-  switch (true) {
-    case weather.some((str) => str.includes("clear") || str.includes("sun")):
-      container.classList.toggle("clear");
-      setImgSrc("sunglasses");
-      weatherText.innerText = `Get your sunnies on. ${city} is looking rather great today.`;
-      break;
-    case weather.some((str) => str.includes("rain")):
-      container.classList.toggle("rain");
-      setImgSrc("umbrella");
-      weatherText.innerText = `Don't forget your umbrella. It's wet in ${city} today.`;
-      break;
-    case weather.some((str) => str.includes("cloud")):
-      container.classList.toggle("cloudy");
-      setImgSrc("cloud");
-      weatherText.innerText = `Light a fire and get cosy. ${city} is looking grey today.`;
-      break;
+// Feature: change style depending on weather
+function setWeatherClass(weatherClass, city) {
+	const weather = weatherClass.split(" ");
+	city = city.charAt(0).toUpperCase() + city.slice(1);
+	switch (true) {
+		case weather.some((str) => str.includes("clear") || str.includes("sun")):
+			container.classList.remove(...container.classList);
+			container.classList.toggle("clear");
+			setImgSrc("sunglasses");
+			weatherText.innerText = `Get your sunnies on. ${city} is looking rather great today.`;
+			break;
+		case weather.some((str) => str.includes("rain")):
+			container.classList.remove(...container.classList);
+			container.classList.toggle("rain");
+			setImgSrc("umbrella");
+			weatherText.innerText = `Don't forget your umbrella. It's wet in ${city} today.`;
+			break;
+		case weather.some((str) => str.includes("cloud")):
+			container.classList.remove(...container.classList);
+			container.classList.toggle("cloudy");
+			setImgSrc("cloud");
+			weatherText.innerText = `Light a fire and get cosy. ${city} is looking grey today.`;
+			break;
 
-    default:
-      container.classList.toggle("default");
-      setImgSrc("cloud");
-  }
+		default:
+			container.classList.remove(...container.classList);
+			container.classList.toggle("default");
+			setImgSrc("cloud");
+			weatherText.innerText = city;
+	}
 }
 
 async function getWeather(city) {
-  try {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_KEY}`
-    );
-    //"https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=162e87975bc70dc88548f3920f1c4fdf"
-    //);
-    const weatherResponse = await response.json();
-    const weatherClass = weatherResponse.weather[0].description;
-    const currentTemperature = Math.round(weatherResponse.main.temp).toFixed(1);
+	try {
+		const response = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_KEY}`
+		);
+		const weatherResponse = await response.json();
+		const weatherClass = weatherResponse.weather[0].description;
+		const currentTemperature = Math.round(weatherResponse.main.temp).toFixed(1);
 
-    const weatherParagraph = document.createElement("p");
-    weatherParagraph.innerHTML += `${weatherClass} | ${currentTemperature}&#176;`;
-    currentWeather.appendChild(weatherParagraph);
+		const weatherParagraph = document.createElement("p");
+		currentWeather.innerHTML = "";
+		weatherParagraph.innerHTML = `${weatherClass} | ${currentTemperature}&#176;`;
+		currentWeather.appendChild(weatherParagraph);
 
-    setSunTimeParagraph("sunrise", weatherResponse.sys.sunrise);
-    setSunTimeParagraph("sunset", weatherResponse.sys.sunset);
-    setWeatherClass(weatherClass.toLowerCase());
-  } catch (err) {
-    console.error(err);
-    currentWeather.innerHTML += `Could not get today's weather for ${city}`;
-  }
+		setSunTimeParagraph("sunrise", weatherResponse.sys.sunrise);
+		setSunTimeParagraph("sunset", weatherResponse.sys.sunset);
+		setWeatherClass(weatherClass.toLowerCase(), city);
+	} catch (err) {
+		console.error(err);
+		setImgSrc("");
+		weatherText.innerText = "Unvalid City, try again";
+		currentWeather.innerHTML = "Unable to load weather";
+	}
 }
 
 // Feature: Weather forecast
 async function getForecast(city) {
-  try {
-    const response = await fetch(forecastURL);
-    const forecast = await response.json();
+	forecastContainer.innerHTML = "";
+	const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=${API_KEY}`;
 
-    const noonWeather = forecast.list.filter((obj) =>
-      obj.dt_txt.includes("12:00:00")
-    );
+	try {
+		const response = await fetch(forecastURL);
+		const forecast = await response.json();
 
-    for (let element of noonWeather) {
-      day = new Date(element.dt_txt)
-        .toLocaleString("en-US", {
-          weekday: "short",
-        })
-        .toLowerCase();
-      forecastContainer.innerHTML += `
+		const noonWeather = forecast.list.filter((obj) =>
+			obj.dt_txt.includes("12:00:00")
+		);
+
+		for (let element of noonWeather) {
+			day = new Date(element.dt_txt)
+				.toLocaleString("en-US", {
+					weekday: "short",
+				})
+				.toLowerCase();
+			forecastContainer.innerHTML += `
 		<li class="forecast-li">
 			<div class="forecast-li-day">${day} </div>
 			<div class="forecast-li-weather">${Math.round(element.main.temp).toFixed(
-        1
-      )}&#176; </div>
+				1
+			)}&#176; </div>
 		</li> 
 	`;
-    }
-  } catch (err) {
-    forecastContainer.innerHTML += "Failed to load forecast";
-    console.error(err);
-  }
+		}
+	} catch (err) {
+		forecastContainer.innerHTML += "Failed to load forecast";
+		console.error(err);
+	}
 }
-
-// search funtion here
-document.addEventListener("DOMContentLoaded", function () {
-  // Your event listener code here
-  searchButton.addEventListener("click", () => {
-    const inputText = searchInput.value.trim();
-    if (inputText) {
-      const newCity = inputText.charAt(0).toUpperCase() + inputText.slice(1);
-      //Log
-      console.log("New City:", newCity);
-      getWeather(newCity);
-      getForecast(newCity);
-    } else {
-      console.log("Please enter a city name.");
-    }
-  });
-});
 
 // Function calls here
 getForecast(city);
 getWeather(city);
+
+// Feature search custom city
+function displayCityWeather(e) {
+	e.preventDefault();
+	const form = new FormData(e.target);
+	city = form.get("city").toLowerCase();
+	getForecast(city);
+	getWeather(city);
+}
